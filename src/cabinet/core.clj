bdbb9115362e2db0e9683228c01ea9cd3eb63621
Jim Sagevid <jim.sagevid@bitsquid.se>
2014-01-16 10:36:51 +0100

    Fixed with-cabinet macro.

--- src/cabinet/core.clj
+++ src/cabinet/core.clj
@@ -2,13 +2,18 @@
   (:use clojure.core)
   (:import (kyotocabinet DB)))
 
+(defn open-cabinet [cabinet]
+  (let [dbmode (cabinet :mode (bit-or kyotocabinet.DB/OREADER kyotocabinet.DB/OWRITER kyotocabinet.DB/OCREATE))
+        filename (cabinet :filename)
+        db (.newInstance kyotocabinet.DB)]
+    (.open db filename dbmode)
+    {:db db :mode dbmode}))
+
+(declare ^:dynamic *kcabinet*) ; used for scoped connections
+
 (defmacro with-cabinet
   "Evaluates forms with a connection to the specified cabinet."
   [cabinet & forms]
-  `(let [dbmode (cabinet :mode (bit-or kyotocabinet.DB/OREADER kyotocabinet.DB/OWRITER kyotocabinet.DB/OCREATE))
-         filename (cabinet :filename)
-         db (.newInstance kyotocabinet.DB)]
-    (.open db filename dbmode)
-    (binding [*kcabinet* {:db db :mode dbmode}]
-      (with-open [db# (*kcabinet* :db)]
-        (do ~@forms)))))
+  `(binding [*kcabinet* (open-cabinet ~cabinet)]
+      (with-open [notused# (*kcabinet* :db)]
+        (do ~@forms))))
